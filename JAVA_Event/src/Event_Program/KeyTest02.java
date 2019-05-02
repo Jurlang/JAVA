d1be43eca37a8bdda0d2a3edddef724a309a8f9d
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2019-05-02 12:48:51 +0900

    방향키에 따라 네모가 움직이게

--- JAVA_Event/src/Event_Program/KeyTest02.java
+++ JAVA_Event/src/Event_Program/KeyTest02.java
@@ -11,6 +11,7 @@ import javax.swing.JLabel;
 @SuppressWarnings("serial")
 public class KeyTest02 extends JFrame implements KeyListener{
 	private JLabel la;
+	int x, y;
 	KeyTest02(){
 		this.setTitle("≈∞∫∏µÂ ¿Ãµø");
 		this.setSize(300,300);
@@ -20,7 +21,9 @@ public class KeyTest02 extends JFrame implements KeyListener{
 		c.addKeyListener(this);
 		c.setLayout(null);
 		la = new JLabel();
-		la.setBounds(10,10,10,10);
+		x = 10;
+		y = 10;
+		la.setBounds(x,y,10,10);
 		c.add(la);
 		la.setOpaque(true);
 		la.setBackground(Color.pink);
@@ -41,28 +44,57 @@ public class KeyTest02 extends JFrame implements KeyListener{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
+//		<<<<<<<<<<<<≥ª∞° ¬ß ƒ⁄µÂ>>>>>>>>>>>>>>>>
+//		int keyCode = e.getKeyCode();
+//		if(keyCode == 37) {
+//			x= la.getX();
+//			y = la.getY();
+//			la.setLocation(x-10, y);
+//		}
+//		else if(keyCode == 38) {
+//			x= la.getX();
+//			y = la.getY();
+//			la.setLocation(x, y-10);
+//		}else if(keyCode == 39) {
+//			x= la.getX();
+//			y = la.getY();
+//			la.setLocation(x+10, y);
+//		}
+//		else if(keyCode == 40) {
+//			x= la.getX();
+//			y = la.getY();
+//			la.setLocation(x, y+10);
+//		}
+//		
+//		<<<<<<<<<<<<Ω‹¿Ã ¬ß ƒ⁄µÂ>>>>>>>>>>>>>>>>
 		int keyCode = e.getKeyCode();
-		int x,y;
-		if(keyCode == 37) {
-			x= la.getX();
-			y = la.getY();
-			la.setLocation(x-10, y);
+		switch(keyCode) {
+		case KeyEvent.VK_UP:
+			if(y-10<0)
+				y=0;
+			else
+				y-=10;
+			break;
+		case KeyEvent.VK_DOWN:
+			if(y+10 > 250)
+				y=250;
+			else
+				y+=10;
+			break;
+		case KeyEvent.VK_LEFT:
+			if(x-10<0)
+				x = 0;
+			else
+				x-=10;
+			break;
+		case KeyEvent.VK_RIGHT:
+			if(x+10>250)
+				x = 250;
+			else
+				x += 10;
+			break;
 		}
-		else if(keyCode == 38) {
-			x= la.getX();
-			y = la.getY();
-			la.setLocation(x, y-10);
-		}else if(keyCode == 39) {
-			x= la.getX();
-			y = la.getY();
-			la.setLocation(x+10, y);
-		}
-		else if(keyCode == 40) {
-			x= la.getX();
-			y = la.getY();
-			la.setLocation(x, y+10);
-		}
-		
+		la.setLocation(x, y);
 	}
 
 	@Override
