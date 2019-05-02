489fc38ef5ee0c1ba5ef96caaac3ea6a09b0de1d
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2019-05-02 09:24:13 +0900

    개인 프로젝트(P_Project) 생성

---
+++ JAVA_P_Project_01/src/School_Info/Basic_Info.java
@@ -0,0 +1,20 @@
+package School_Info;
+
+public class Basic_Info {
+	String name;
+	String addr;
+	int age;
+	boolean sex;
+	String idnum;
+	
+	public Basic_Info() {}
+	public Basic_Info(String name, int age, String idnum, String addr) {
+		
+	}
+	
+	void setName(String name)	{	this.name = name;	}
+	void setAddr(String addr)	{	this.addr = addr;	}
+	void setAge(int age)		{	this.age = age;		}
+	void setIdnum(String idnum)	{	this.idnum = idnum; 
+									if()}
+}
