32764041a6198d07c445fadb467f70bc3d96ea12
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2019-05-03 15:05:35 +0900

    TextArea 메소드 예제

---
+++ JAVA_SwingComponent/src/JTextAreaMethod.java
@@ -0,0 +1,41 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+@SuppressWarnings("serial")
+public class JTextAreaMethod extends JFrame{
+	JTextField tf;
+	JTextArea ta;
+	
+	JTextAreaMethod(){
+		this.setTitle("제목없음");
+		this.setSize(300,300);
+		
+		tf = new JTextField();
+		ta = new JTextArea();
+		add(tf, BorderLayout.NORTH);
+		add(new JScrollPane(ta));
+		tf.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ta.append(tf.getText()+"\n");
+				tf.setText("");
+			}
+			
+		});
+		
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+	public static void main(String[] args) {
+		new JTextAreaMethod();
+	}
+
+}
